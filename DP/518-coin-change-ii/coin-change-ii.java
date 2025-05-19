1033cb5ecac6b79439ced16644496acf623403e6
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-05-19 16:31:28 +0530

    Time: 17 ms (20.53%) | Memory: 55.7 MB (5.05%) - LeetSync

--- DP/518-coin-change-ii/coin-change-ii.java
+++ DP/518-coin-change-ii/coin-change-ii.java
@@ -3,7 +3,8 @@ class Solution {
         int[][] dp = new int[amount+1][coins.length];
         for(int[] a:dp) Arrays.fill(a,-1);
         // return coin(coins,amount,0);
-        return coinTD(coins,amount,0,dp);
+        // return coinTD(coins,amount,0,dp);
+        return coinBU(coins,amount);
     }
     public int coin(int[] coin,int am,int i){
         if(am==0) return 1;
@@ -22,6 +23,20 @@ class Solution {
         if(am>=coin[i]) pick = coinTD(coin,am-coin[i],i,dp);
         noPick = coinTD(coin,am,i+1,dp);
         return dp[am][i]=pick+noPick;
-
+    }
+    public int coinBU(int[] coin,int amnt){
+        int[][] dp = new int[amnt+1][coin.length+1];
+        Arrays.fill(dp[0],1);
+        for(int am=1;am<dp.length;am++){
+            for(int j=1;j<dp[0].length;j++){
+                int pick = 0, noPick = 0;
+                if(am>=coin[j-1]){
+                    pick = dp[am-coin[j-1]][j];
+                }
+                noPick = dp[am][j-1];
+                dp[am][j] = pick+noPick;
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
     }
 }
