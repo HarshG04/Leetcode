3e332edff98836d959314da32feb5c852d17ce2d
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-05-22 17:52:36 +0530

    Time: 4 ms (62.33%) | Memory: 47.8 MB (33.04%) - LeetSync

--- DP/64-minimum-path-sum/minimum-path-sum.java
+++ DP/64-minimum-path-sum/minimum-path-sum.java
@@ -16,18 +16,19 @@ class Solution {
         return dp[i][j] = Math.min(down,right)+grid[i][j];
     }
     public int minPathBU(int[][] grid){
-        for (int i = 1; i < grid[0].length; i++) {
+        int n = grid.length, m = grid[0].length;
+        for (int i = 1; i <m; i++) {
             grid[0][i] += grid[0][i-1];
         }
-        for (int i = 1; i<grid.length; i++) {
+        for (int i = 1; i<n; i++) {
             grid[i][0] += grid[i-1][0];
         }
         
-        for(int i=1;i<grid.length;i++){
-            for(int j=1;j<grid[0].length;j++){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1]);
             }
         }
-        return grid[grid.length-1][grid[0].length-1];
+        return grid[n-1][m-1];
     }
 }
