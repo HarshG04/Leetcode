e8ff24c6df7d0df69f58e5a907aaa08d87255293
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-07-03 19:18:48 +0530

    Time: 5 ms (92.38%) | Memory: 44.3 MB (55.72%) - LeetSync

--- DP/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ DP/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return lis(nums);
+        return lis2(nums);
     }
     public int lis(int[] arr){
         if(arr.length==1) return 1;
@@ -19,5 +19,29 @@ class Solution {
             }
         }
         return max;
+    }
+    public int lis2(int[] arr){
+        if(arr.length==1) return 1;
+        ArrayList<Integer> l = new ArrayList<>();
+        int n = arr.length;
+        l.add(arr[0]);
+        for(int i=1;i<n;i++){
+            if(arr[i]>l.get(l.size()-1)) l.add(arr[i]);
+            else{
+                int idx = set(l,arr[i]);
+                l.set(idx,arr[i]);
+            }
+        }
+        return l.size();
+    }
+    public int set(ArrayList<Integer> ll,int val){
+        int l = 0, r = ll.size()-1;
+        while(l<r){
+            int mid = (l+r)/2;
+            if(ll.get(mid)==val) return mid;
+            else if(ll.get(mid)<val) l = mid+1;
+            else r = mid;
+        }
+        return l;
     }
 }
