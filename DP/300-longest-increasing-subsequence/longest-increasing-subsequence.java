13ab3642b6471b37ffd08f531c1f2a29f9b79730
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-07-03 19:05:35 +0530

    Time: 53 ms (30.12%) | Memory: 44.3 MB (55.72%) - LeetSync

--- DP/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ DP/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,46 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return LIS1(nums);
+        return lis(nums);
     }
-    
-    public int LIS(int[] nums){
-        int n = nums.length;
-        int[] dp = new int[nums.length];
+    public int lis(int[] arr){
+        if(arr.length==1) return 1;
+        int n = arr.length;
+        int[] dp = new int[n];
         Arrays.fill(dp,1);
+        int max = 1;
         for(int i=1;i<n;i++){
-            for(int j=i-1;j>=0;j--){
-                if(nums[j]<nums[i]){
-                    dp[i] = Math.max(dp[i],dp[j]);
+            int j=i-1;
+            while(j>=0){
+                if(arr[j]<arr[i]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                    max = Math.max(max,dp[i]);
                 }
+                j--;
             }
         }
-        int max = dp[0];
-        for(int i:dp) max = Math.max(max,i);
         return max;
-    }
-
-    public static int LIS1(int[] nums){
-        int[] arr = new int[nums.length];
-        arr[0] = nums[0];
-        int idx = 1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>arr[idx-1]) arr[idx++] = nums[i];
-            else{
-                int j = search(arr,nums[i],idx);
-                arr[j] = nums[i];
-            }
-        }
-        return idx;
-    }
-    public static int search(int[] arr,int val,int j){
-        int i = 0;
-        while(i<=j){
-            int mid = (i+j)/2;
-            if(arr[mid]>val) j = mid-1;
-            else if(arr[mid]<val) i = mid+1;
-            else return mid;
-        }
-//        System.out.println("Right : "+val+" " +i);
-        return i;
     }
 }
