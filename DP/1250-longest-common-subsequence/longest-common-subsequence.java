3485536f8ddfe71ed1e5410de47251bcb718a345
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-07-04 12:16:35 +0530

    Time: 20 ms (73.99%) | Memory: 51 MB (56.55%) - LeetSync

--- DP/1250-longest-common-subsequence/longest-common-subsequence.java
+++ DP/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,10 +1,32 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
-        for(int[] ar:dp){
-            Arrays.fill(ar,-1);
+        // int[][] dp = new int[text1.length()][text2.length()];
+        // for(int[] ar:dp){
+        //     Arrays.fill(ar,-1);
+        // }
+        // return lcsTD(text1,text2,0,0,dp);
+
+        return lcsBU(text1,text2);
+    }
+
+    public static int lcsBU(String s1,String s2){
+        int[][] dp = new int[s2.length()+1][s1.length()+1];
+
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(s1.charAt(j-1)==s2.charAt(i-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
-        return lcsTD(text1,text2,0,0,dp);
+
+        return dp[dp.length-1][dp[0].length-1];
+        
+        
+        
+        
     }
 
     public static int lcsTD(String s1,String s2,int i,int j,int[][] dp){
