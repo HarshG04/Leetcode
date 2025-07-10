56cf93b4fd6fec7e197b361f16d70f3bdf3830b1
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-07-10 18:39:55 +0530

    Time: 6 ms (11.91%) | Memory: 47.8 MB (32.15%) - LeetSync

--- DP/64-minimum-path-sum/minimum-path-sum.java
+++ DP/64-minimum-path-sum/minimum-path-sum.java
@@ -1,34 +1,28 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        // int[][] dp = new int[grid.length][grid[0].length];
-        // for(int[] a:dp){
-        //     Arrays.fill(a,-1);
-        // }
-        // return minPathTD(grid,dp,0,0);
-        return minPathBU(grid);
+        // return minpath(grid,0,0,0);
+        return minpathBU(grid);
     }
-    public int minPathTD(int[][] grid,int[][] dp,int i,int j){
-        if(i==grid.length-1 && j==grid[0].length-1) return grid[i][j];
-        if(i>=grid.length || j>=grid[0].length) return Integer.MAX_VALUE;
-        if(dp[i][j]>-1) return dp[i][j];
-        int down = minPathTD(grid,dp,i+1,j);
-        int right = minPathTD(grid,dp,i,j+1);
-        return dp[i][j] = Math.min(down,right)+grid[i][j];
-    }
-    public int minPathBU(int[][] grid){
-        int n = grid.length, m = grid[0].length;
-        for (int i = 1; i <m; i++) {
-            grid[0][i] += grid[0][i-1];
-        }
-        for (int i = 1; i<n; i++) {
-            grid[i][0] += grid[i-1][0];
-        }
-        
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1]);
+    public int minpathBU(int[][] grid){
+        int[][] dp=new int[grid.length][grid[0].length];
+        dp[0][0]=grid[0][0];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(i==0 && j==0) continue;
+                int x=Integer.MAX_VALUE,y=x;
+                if(i>0) x=dp[i-1][j];
+                if(j>0) y=dp[i][j-1];
+                dp[i][j]=Math.min(x,y)+grid[i][j];
             }
         }
-        return grid[n-1][m-1];
+        return dp[dp.length-1][dp[0].length-1];
+    }
+    public int minpath(int[][]grid,int i,int j,int sum){
+        if(i==grid.length-1 && j==grid[0].length-1) return sum+grid[i][j];
+        if(j>=grid[0].length || i>=grid.length) return Integer.MAX_VALUE;
+        sum+=grid[i][j];
+        int dwn = minpath(grid,i+1,j,sum);
+        int rgt = minpath(grid,i,j+1,sum);
+        return Math.min(dwn,rgt);
     }
 }
