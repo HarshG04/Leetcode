e12343cb2d5501ed1a9ac6523f9f3b55afd3a5d6
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-07-16 12:59:52 +0530

    Time: 1 ms (99.72%) | Memory: 45.3 MB (65.15%) - LeetSync

--- DP/967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ DP/967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,22 +1,35 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int[][] dp = new int[matrix.length][matrix[0].length];
-        for(int[] a:dp){
-            Arrays.fill(a,-200);
+        // int ans = Integer.MAX_VALUE;
+        // for(int i=0;i<matrix[0].length;i++){
+        //     ans = Math.min(minfall(matrix,0,i),ans);
+        // }
+        // return ans;
+
+        int[][]dp=new int[matrix.length][matrix[0].length];
+        for(int[]a:dp) Arrays.fill(a,-100000);
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<matrix[0].length;i++){
+            ans = Math.min(minfallTD(matrix,0,i,dp),ans);
         }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < dp.length; i++) {
-            min = Math.min(min,minPathTD(matrix,dp,0,i));
-        }
-        return min;
+        return ans;
+        
     }
-    public int minPathTD(int[][] grid,int[][] dp,int i,int j){
-        if(i<0 || i>=grid.length || j<0 || j>=grid.length) return Integer.MAX_VALUE;
-        if(i==grid.length-1) return grid[i][j];
-        if(dp[i][j]!=-200) return dp[i][j];
-        int lft = minPathTD(grid,dp,i+1,j-1);
-        int down = minPathTD(grid,dp, i+1, j);
-        int rgt = minPathTD(grid,dp,i+1, j+1);
-        return dp[i][j] = Math.min(lft,Math.min(down,rgt)) + grid[i][j];
+    public int minfallTD(int[][] arr,int i,int j,int[][]dp){
+        if(j>=arr[0].length || j<0) return Integer.MAX_VALUE;
+        if(i==arr.length-1) return arr[i][j];
+        if(dp[i][j]>-100000) return dp[i][j];
+        int lft = minfallTD(arr,i+1,j-1,dp);
+        int dwn = minfallTD(arr,i+1,j,dp);
+        int rgt = minfallTD(arr,i+1,j+1,dp);
+        return dp[i][j]=Math.min(lft,Math.min(dwn,rgt))+arr[i][j];
+    }
+    public int minfall(int[][] arr,int i,int j){
+        if(j>=arr[0].length || j<0) return Integer.MAX_VALUE;
+        if(i==arr.length-1) return arr[i][j];
+        int lft = minfall(arr,i+1,j-1);
+        int dwn = minfall(arr,i+1,j);
+        int rgt = minfall(arr,i+1,j+1);
+        return Math.min(lft,Math.min(dwn,rgt))+arr[i][j];
     }
 }
