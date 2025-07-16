c3a96e13614cc6141b55e8b8f05bd7fe3afcce46
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-07-16 13:23:42 +0530

    Time: 228 ms (9.05%) | Memory: 51.7 MB (34.95%) - LeetSync

--- DP/1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ DP/1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -1,22 +1,25 @@
 class Solution {
-    public int minFallingPathSum(int[][] matrix) {
-     int[][] dp = new int[matrix.length][matrix[0].length];
-        for(int[] a:dp){
-            Arrays.fill(a,-100);
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < dp[0].length; i++) {
-            min = Math.min(min,pathSum(matrix,dp,0,i));
-        }
-        return min;
-    }
-    public int pathSum(int[][] grid,int[][] dp,int i,int j){
-        if(i==grid.length-1) return grid[i][j];
-        if(dp[i][j]!=-100) return dp[i][j];
+    public int minFallingPathSum(int[][] grid) {
+    int[][]dp=new int[grid.length][grid[0].length];
+        for(int[]a:dp) Arrays.fill(a,-100000);
         int ans = Integer.MAX_VALUE;
-        for(int k=0;k<grid[0].length;k++){
-            if(k!=j) ans = Math.min(ans,pathSum(grid, dp, i+1, k));
+        for(int i=0;i<grid[0].length;i++){
+            ans = Math.min(minfallTD(grid,0,i,dp),ans);
         }
-        return dp[i][j] = ans+grid[i][j];
+        return ans;
+        
+    }
+    public int minfallTD(int[][] arr,int i,int j,int[][]dp){
+        if(j>=arr[0].length || j<0) return Integer.MAX_VALUE;
+        if(i==arr.length-1) return arr[i][j];
+        if(dp[i][j]>-100000) return dp[i][j];
+        int ans = Integer.MAX_VALUE;
+        for(int k=0;k<arr[0].length;k++){
+            if(k!=j) ans=Math.min(ans,minfallTD(arr,i+1,k,dp));
+        }
+        // int lft = minfallTD(arr,i+1,j-1,dp);
+        // int dwn = minfallTD(arr,i+1,j,dp);
+        // int rgt = minfallTD(arr,i+1,j+1,dp);
+        return dp[i][j]=ans+arr[i][j];
     }
 }
