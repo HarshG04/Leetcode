d8973e0e2ed7e9b919372b43f53bef8d38088ddc
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-08-02 19:17:29 +0530

    Time: 31 ms (10.78%) | Memory: 51.6 MB (6.10%) - LeetSync

--- DP/1250-longest-common-subsequence/longest-common-subsequence.java
+++ DP/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,59 +1,15 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        // int[][] dp = new int[text1.length()][text2.length()];
-        // for(int[] ar:dp){
-        //     Arrays.fill(ar,-1);
-        // }
-        // return lcsTD(text1,text2,0,0,dp);
-
-        return lcsBU(text1,text2);
+        int[][]dp=new int[text1.length()][text2.length()];
+        for(int[]a:dp) Arrays.fill(a,-1);
+        return lcs(text1,text2,0,0,dp);
     }
-
-    public static int lcsBU(String s1,String s2){
-        int[][] dp = new int[s2.length()+1][s1.length()+1];
-
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[0].length;j++){
-                if(s1.charAt(j-1)==s2.charAt(i-1)){
-                    dp[i][j] = dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-
-        return dp[dp.length-1][dp[0].length-1];
-        
-        
-        
-        
-    }
-
-    public static int lcsTD(String s1,String s2,int i,int j,int[][] dp){
-        if(i>=s1.length() || j>=s2.length()) return 0;
+    public int lcs(String s1,String s2,int i,int j,int[][]dp){
+        if(j==s2.length() || i==s1.length()) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int count = 0;
-        if(s1.charAt(i)==s2.charAt(j)){
-            count += 1 + lcsTD(s1,s2,i+1,j+1,dp);
-        }else{
-            int a = lcsTD(s1,s2,i,j+1,dp);
-            int b = lcsTD(s1,s2,i+1,j,dp);
-            count += Math.max(a,b);
-        }
-        return dp[i][j]=count;
-    }
-
-
-    public static int lcs(String s1,String s2,int i,int j){
-        if(i>=s1.length() || j>=s2.length()) return 0;
-        int count = 0;
-        if(s1.charAt(i)==s2.charAt(j)){
-            count += 1 + lcs(s1,s2,i+1,j+1);
-        }else{
-            int a = lcs(s1,s2,i,j+1);
-            int b = lcs(s1,s2,i+1,j);
-            count += Math.max(a,b);
-        }
-        return count;
+        int ans=0;
+        if(s1.charAt(i)==s2.charAt(j)) ans = lcs(s1,s2,i+1,j+1,dp)+1;
+        else ans = Math.max(lcs(s1,s2,i+1,j,dp),lcs(s1,s2,i,j+1,dp));
+        return dp[i][j]=ans;
     }
 }
