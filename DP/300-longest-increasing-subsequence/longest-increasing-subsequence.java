9e2f433b3502833324c2a5c801a216252a7e7ab8
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-08-02 17:21:30 +0530

    Time: 47 ms (39.63%) | Memory: 44 MB (87.76%) - LeetSync

--- DP/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ DP/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,47 +1,19 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return lis2(nums);
+        if(nums.length==1) return 1;
+        return lis(nums);
     }
-    public int lis(int[] arr){
-        if(arr.length==1) return 1;
-        int n = arr.length;
+    public int lis(int[] nums){
+        int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp,1);
-        int max = 1;
-        for(int i=1;i<n;i++){
-            int j=i-1;
-            while(j>=0){
-                if(arr[j]<arr[i]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                    max = Math.max(max,dp[i]);
-                }
-                j--;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]>nums[j]) dp[i] = Math.max(dp[i],dp[j]+1);
+                max = Math.max(max,dp[i]);
             }
         }
         return max;
-    }
-    public int lis2(int[] arr){
-        if(arr.length==1) return 1;
-        ArrayList<Integer> l = new ArrayList<>();
-        int n = arr.length;
-        l.add(arr[0]);
-        for(int i=1;i<n;i++){
-            if(arr[i]>l.get(l.size()-1)) l.add(arr[i]);
-            else{
-                int idx = set(l,arr[i]);
-                l.set(idx,arr[i]);
-            }
-        }
-        return l.size();
-    }
-    public int set(ArrayList<Integer> ll,int val){
-        int l = 0, r = ll.size()-1;
-        while(l<r){
-            int mid = (l+r)/2;
-            if(ll.get(mid)==val) return mid;
-            else if(ll.get(mid)<val) l = mid+1;
-            else r = mid;
-        }
-        return l;
     }
 }
