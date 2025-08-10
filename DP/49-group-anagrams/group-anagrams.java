4f371548be4ff657c7825d6f368a8951110f1ce9
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-08-10 18:19:19 +0530

    Time: 799 ms (5.02%) | Memory: 49.1 MB (10.97%) - LeetSync

--- DP/49-group-anagrams/group-anagrams.java
+++ DP/49-group-anagrams/group-anagrams.java
@@ -1,19 +1,25 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map = new HashMap<>();
-        for(int i=0;i<strs.length;i++){
-            String s = strs[i];
-            int[] freq = new int[26];
-            for(int j=0;j<s.length();j++) freq[s.charAt(j)-97]++;
-            StringBuilder sb = new StringBuilder(Arrays.toString(freq));
-            String key = sb.toString();
-            System.out.println(s+" "+key);
-            if(!map.containsKey(key)){
-                map.put(key,new ArrayList<>());
-            }
-            map.get(key).add(s);
+    public List<List<String>> groupAnagrams(String[] str) {
+        HashMap<String,ArrayList<String>> map = new HashMap<>();
+        for(int i=0;i<str.length;i++){
+            String key = makeKey(str[i]);
+            System.out.println(key);
+            if(!map.containsKey(key)) map.put(key,new ArrayList<>());
+            map.get(key).add(str[i]);
         }
-        
         return new ArrayList<>(map.values());
+    }
+    public String makeKey(String s){
+        int[] freq = new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-97]++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<26;i++) {
+            System.out.print(freq[i]);
+            sb.append(freq[i]).append('$');
+        }
+        System.out.println();
+        return sb.toString();
     }
 }
