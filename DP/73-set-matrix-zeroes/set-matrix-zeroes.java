0011884441f2736283bbb0f41383cb1acf6bfdc2
Harsh Goyal <122627106+HarshG04@users.noreply.github.com>
2025-09-12 05:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (16.39%) - LeetSync

--- DP/73-set-matrix-zeroes/set-matrix-zeroes.java
+++ DP/73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,30 +1,57 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+        boolean firstRow = false, firstCol = false;
+        // checking first row if having zero
+        for(int i=0;i<matrix[0].length;i++){
+            if(matrix[0][i]==0){
+                firstRow = true;
+                break;
+            }
+        }
+        // checking first coloumn if having zero
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+            if(matrix[i][0]==0){
+                firstCol = true;
+                break;
+            }
+        }
+        // checking from 1,1
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    if(map.get(i)==null) map.put(i,new ArrayList<>());
-                    map.get(i).add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        // setting rows to zero
+        for(int i=1;i<matrix.length;i++){
+            if(matrix[i][0]==0){
+                for(int j=1;j<matrix[0].length;j++){
+                    matrix[i][j] = 0;
                 }
             }
         }
 
-        for(int r : map.keySet()){
-            for(int c : map.get(r)){
-                setZeros(matrix,r,c);
+        // setting coloumn to zeros
+        for(int i=1;i<matrix[0].length;i++){
+            if(matrix[0][i]==0){
+                for(int j=1;j<matrix.length;j++){
+                    matrix[j][i] = 0;
+                }
             }
         }
- 
-    }
-    public void setZeros(int[][] arr,int r,int c){
-        for(int i=0;i<arr[0].length;i++){
-            arr[r][i] = 0;
+
+        if(firstRow){
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[0][i]=0;
+            }
         }
-        for(int i=0;i<arr.length;i++){
-            arr[i][c] = 0;
+        if(firstCol){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0]=0;
+            }
         }
         
-
     }
 }
